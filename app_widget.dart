7c00a231368caca608e6a1277c127f18f9b40d13
Carlos Pereira <111026070+snntxjunior@users.noreply.github.com>
2023-12-19 11:55:24 -0300

    app_widget.dart

--- app_widget.dart
+++ app_widget.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:statefulclickcounter/app_controller.dart';
 
 import 'home_page.dart';
+import 'login_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: AppController.instance.isDartTheme
+                ? Brightness.light
+                : Brightness.light,
+          ),
+          home: LoginPage(),
+        );
+      },
     );
   }
 }
